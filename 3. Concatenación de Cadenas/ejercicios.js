eb4aa663dbbcc291ed75e2669360f46d116ad059
Cristian Budano <caebar@gmail.com>
2024-06-25 20:29:45 -0300

    Tercer Ejercicio Solicitado

--- 3. Concatenación de Cadenas/ejercicios.js
+++ 3. Concatenación de Cadenas/ejercicios.js
@@ -1,1 +1,11 @@
 // Ejercicio 3.1: Declara dos variables, 'saludo' y 'nombre', y asígnales los valores "Hola" y tu nombre respectivamente. Usa el operador de concatenación para unir estas dos cadenas y muestra el resultado en la consola.
+
+let saludo = "Hola";
+
+let nombre = "Cristian Armando Emmanuel Budano";
+
+const resultado = saludo + " " + nombre;
+
+console.log(resultado);
+
+/**************************************************/
