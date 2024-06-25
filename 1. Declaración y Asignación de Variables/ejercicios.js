da3ca89d15fae9fc25326b194540b0c802868fc6
Cristian Budano <caebar@gmail.com>
2024-06-25 20:08:55 -0300

    Primer Ejercicio Solicitado

--- 1. Declaración y Asignación de Variables/ejercicios.js
+++ 1. Declaración y Asignación de Variables/ejercicios.js
@@ -1,3 +1,24 @@
 // Ejercicio 1.1: Declara una variable llamada 'nombre' y asígnale tu nombre.
+
+let nombre = 'Cristian Armando Emmanuel';
+let apellido = ' Budano';
+
+/***************************************/
+
+
 // Ejercicio 1.2: Declara una variable llamada 'edad' y asígnale tu edad.
+
+var edad = 41;
+
+/*************************************/
+
 // Ejercicio 1.3: Declara una variable llamada 'esEstudiante' y asígnale el valor 'true' si eres estudiante o 'false' si no lo eres.
+
+let esEstudiante = false;
+
+/*************************************/
+
+console.log(nombre + apellido);
+console.log(edad);
+console.log(esEstudiante);
+
