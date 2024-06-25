0ccb027d185c06498785eb318257776003335d07
Cristian Budano <caebar@gmail.com>
2024-06-25 20:16:14 -0300

    Segundo Ejercicio Solicitado

--- 2. Operadores Aritméticos/ejercicios.js
+++ 2. Operadores Aritméticos/ejercicios.js
@@ -1,6 +1,35 @@
+
 // Ejercicio 2.1: Declara dos variables, 'a' y 'b', asígnales los valores 5 y 3 respectivamente. Calcula y muestra en la consola el resultado de:
+
+let a = 5;
+var b = 3;
+
+/***********************************************/
+
 // - La suma de 'a' y 'b'.
+
+console.log(a + b);
+
+/***********************************************/
+
 // - La resta de 'a' y 'b'.
+
+console.log(a - b);
+
+/***********************************************/
+
 // - La multiplicación de 'a' y 'b'.
+
+console.log(a * b);
+
 // - La división de 'a' entre 'b'.
+
+console.log(a / b);
+
+/***********************************************/
+
 // - El módulo de 'a' y 'b'.
+
+console.log(a % b);
+
+/**************************************/
