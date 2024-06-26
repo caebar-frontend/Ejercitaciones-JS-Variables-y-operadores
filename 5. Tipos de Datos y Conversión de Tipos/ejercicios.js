c61aec36d31da93ec12c536b6e88f0a505934788
Cristian Budano <caebar@gmail.com>
2024-06-26 17:02:47 -0300

    Ejercicio Cinco Solicitado Hecho

--- 5. Tipos de Datos y Conversión de Tipos/ejercicios.js
+++ 5. Tipos de Datos y Conversión de Tipos/ejercicios.js
@@ -1,2 +1,26 @@
 // Ejercicio 5.1: Declara una variable 'cadenaNumero' y asígnale el valor "123". Convierte esta cadena a un número y almacénalo en una nueva variable 'numeroConvertido'. Muestra ambos valores en la consola.
+
+let cadenaNumero = '123';
+let numeroConvertido = Number.parseInt(cadenaNumero);
+
+
+console.log("El numero en formato cadena es: " + cadenaNumero);
+console.log("El numero en formato numérico es: " + numeroConvertido);
+
+/******************************************* */
+
+console.log("///////////////////////////////////////////////////////////////////////");
+
 // Ejercicio 5.2: Declara una variable 'booleano' y asígnale el valor 'true'. Convierte este valor a una cadena y almacénalo en una nueva variable 'cadenaBooleano'. Muestra ambos valores en la consola.
+
+let booleano = true;
+
+let cadenaBooleano = String(booleano);
+
+console.log("El valor del booleano es: " + booleano);
+console.log("El booleano en formato cadena es: " + cadenaBooleano);
+
+
+/******************************************* */
+
+
